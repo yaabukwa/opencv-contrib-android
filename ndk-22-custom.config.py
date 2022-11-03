a22e3fd04ec23a6dd3a6bfee0d343910dea05e04
Youssef Abukwaik <98774884+yaabukwa@users.noreply.github.com>
2022-11-03 09:41:40 -0700

    Update ndk-22-custom.config.py

--- ndk-22-custom.config.py
+++ ndk-22-custom.config.py
@@ -5,14 +5,14 @@ COMMON_CMAKE_VAR = dict(
                 WITH_OPENJPEG='OFF',
                 WITH_OPENEXR='OFF',
                 WITH_JASPER='OFF',
-                DWITH_JPEG='OFF',
+                WITH_JPEG='OFF',
                 packbits='OFF',
                 WITH_PROTOBUF='OFF',
                 mdi='OFF')
 
 ABIs = [
-    ABI("2", "armeabi-v7a", None, cmake_vars=dict(ANDROID_ABI='armeabi-v7a with NEON', ANDROID_GRADLE_PLUGIN_VERSION='7.2.0', GRADLE_VERSION='7.5', KOTLIN_PLUGIN_VERSION='1.5.10')),
-    ABI("3", "arm64-v8a",   None, cmake_vars=dict(ANDROID_GRADLE_PLUGIN_VERSION='7.2.0', GRADLE_VERSION='7.5', KOTLIN_PLUGIN_VERSION='1.5.10')),
+    ABI("2", "armeabi-v7a", None, cmake_vars=dict(ENABLE_NEON='ON', ANDROID_ABI='armeabi-v7a with NEON', ANDROID_GRADLE_PLUGIN_VERSION='7.2.0', GRADLE_VERSION='7.5', KOTLIN_PLUGIN_VERSION='1.5.10')),
+    ABI("3", "arm64-v8a",   None, cmake_vars=dict(ENABLE_NEON='ON', ANDROID_GRADLE_PLUGIN_VERSION='7.2.0', GRADLE_VERSION='7.5', KOTLIN_PLUGIN_VERSION='1.5.10')),
     ABI("5", "x86_64",      None, cmake_vars=dict(ANDROID_GRADLE_PLUGIN_VERSION='7.2.0', GRADLE_VERSION='7.5', KOTLIN_PLUGIN_VERSION='1.5.10')),
     ABI("4", "x86",         None, cmake_vars=dict(ANDROID_GRADLE_PLUGIN_VERSION='7.2.0', GRADLE_VERSION='7.5', KOTLIN_PLUGIN_VERSION='1.5.10')),
 ]
