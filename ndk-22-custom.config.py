8c3ae6dc9913945c460df162e6af8462cb4151c2
Youssef Abukwaik <98774884+yaabukwa@users.noreply.github.com>
2022-11-07 11:04:15 -0800

    Update ndk-22-custom.config.py

--- ndk-22-custom.config.py
+++ ndk-22-custom.config.py
@@ -1,10 +1,14 @@
 COMMON_CMAKE_VAR = dict(
                 CMAKE_BUILD_TYPE='Release',
                 CMAKE_CONFIGURATION_TYPES='Release',
-                ANDROID_STL='c++_static',              
+                CMAKE_CXX_FLAGS='-O3',
+                CMAKE_C_FLAGS='-O3'
                 OPENCV_DISABLE_FILESYSTEM_SUPPORT='ON',
                 WITH_ITT='OFF',
+                WITH_TBB='ON',
+                BUILD_TBB='ON',
                 BUILD_FAT_JAVA_LIB='ON',
+                WITH_EIGEN='ON',
                 WITH_OPENJPEG='OFF',
                 WITH_OPENEXR='OFF',
                 WITH_JASPER='OFF',
@@ -14,10 +18,10 @@ COMMON_CMAKE_VAR = dict(
                 mdi='OFF')
 
 ABIs = [
-    ABI("2", "armeabi-v7a", None, cmake_vars=dict(ENABLE_NEON='ON', ANDROID_ABI='armeabi-v7a with NEON', ANDROID_GRADLE_PLUGIN_VERSION='7.2.0', GRADLE_VERSION='7.5', KOTLIN_PLUGIN_VERSION='1.5.10', CMAKE_CXX_FLAGS='-O3', CMAKE_C_FLAGS='-O3')),
-    ABI("3", "arm64-v8a",   None, cmake_vars=dict(ENABLE_NEON='ON', ANDROID_GRADLE_PLUGIN_VERSION='7.2.0', GRADLE_VERSION='7.5', KOTLIN_PLUGIN_VERSION='1.5.10', CMAKE_CXX_FLAGS='-O3', CMAKE_C_FLAGS='-O3')),
-    ABI("5", "x86_64",      None, cmake_vars=dict(ANDROID_GRADLE_PLUGIN_VERSION='7.2.0', GRADLE_VERSION='7.5', KOTLIN_PLUGIN_VERSION='1.5.10')),
-    ABI("4", "x86",         None, cmake_vars=dict(ANDROID_GRADLE_PLUGIN_VERSION='7.2.0', GRADLE_VERSION='7.5', KOTLIN_PLUGIN_VERSION='1.5.10')),
+    ABI("2", "armeabi-v7a", None, cmake_vars=dict(ENABLE_THIN_LTO='ON', ENABLE_NEON='ON', ENABLE_VFPV3='ON', ENABLE_THIN_LTO='ON', ANDROID_ABI='armeabi-v7a with NEON', ANDROID_GRADLE_PLUGIN_VERSION='7.2.0', GRADLE_VERSION='7.5', KOTLIN_PLUGIN_VERSION='1.5.10')),
+    ABI("3", "arm64-v8a",   None, cmake_vars=dict(ENABLE_THIN_LTO='ON', ENABLE_NEON='ON', ENABLE_VFPV3='ON', ENABLE_THIN_LTO='ON', ANDROID_GRADLE_PLUGIN_VERSION='7.2.0', GRADLE_VERSION='7.5', KOTLIN_PLUGIN_VERSION='1.5.10')),
+    ABI("5", "x86_64",      None, cmake_vars=dict(ENABLE_LTO='ON', ANDROID_GRADLE_PLUGIN_VERSION='7.2.0', GRADLE_VERSION='7.5', KOTLIN_PLUGIN_VERSION='1.5.10')),
+    ABI("4", "x86",         None, cmake_vars=dict(ENABLE_LTO='ON', ANDROID_GRADLE_PLUGIN_VERSION='7.2.0', GRADLE_VERSION='7.5', KOTLIN_PLUGIN_VERSION='1.5.10')),
 ]
 
 for i, abi in enumerate(ABIs):
